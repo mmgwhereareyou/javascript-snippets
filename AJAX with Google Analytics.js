f4f0696df39fd20c8131ef076b71192ef1dfd736
mmgwhereareyou <michaelmegee@hotmail.com>
2015-03-03 18:50:43 -0600

    Comments Removed

--- AJAX with Google Analytics.js
+++ AJAX with Google Analytics.js
@@ -2,32 +2,27 @@
 // AJAX
 //
 window.addEventListener( "popstate", function(e) {
-    //$("#contentWrapper").animate( { opacity: "toggle" }, "slow" , function () {
-        $.get( "AJAXContent.php?contentIdentifier="+((window.location.hash.replace( "#", "")!="")?window.location.hash.replace( "#", ""):"Home"), function ( responseHTML ) {
-            $("#contentWrapper").html( responseHTML );
-            window.scrollTo(0,0);
-            document.title = "Prefix Text " + $("h2").text(); // Set's Header on Page with Title
-            // Google Analytics
-            ga('send', {
-              'hitType': 'pageview',
-              'page': window.location.href
-            });
-        });
-        //$("#contentWrapper").animate( { opacity: "toggle" }, "slow", function () {});
-    //});
+  $.get( "AJAXContent.php?contentIdentifier="+((window.location.hash.replace( "#", "")!="")?window.location.hash.replace( "#", ""):"Home"), function ( responseHTML ) {
+      $("#contentWrapper").html( responseHTML );
+      window.scrollTo(0,0);
+      document.title = "Prefix Text " + $("h2").text(); // Set's Header on Page with Title
+      // Google Analytics
+      ga('send', {
+        'hitType': 'pageview',
+        'page': window.location.href
+      });
+  });
 });
 function goto ( contentIdentifier ) {
-    //$("#contentWrapper").animate( { height: "toggle", opacity: "toggle" }, "slow" , function () {
-        $.get( "AJAXContent.php?contentIdentifier="+contentIdentifier, function ( responseHTML ) {
-            window.history.pushState( "", "", "#" + contentIdentifier );
-            $("#contentWrapper").html(responseHTML);
-            window.scrollTo(0,0);
-            document.title = "Michael Megee " + $("h2").text(); // Set's Header on Page with Title
-            // Google Analytics
-            ga('send', {
-              'hitType': 'pageview',
-              'page': window.location.href
-            });
-        });
-    //});
+  $.get( "AJAXContent.php?contentIdentifier="+contentIdentifier, function ( responseHTML ) {
+      window.history.pushState( "", "", "#" + contentIdentifier );
+      $("#contentWrapper").html(responseHTML);
+      window.scrollTo(0,0);
+      document.title = "Michael Megee " + $("h2").text(); // Set's Header on Page with Title
+      // Google Analytics
+      ga('send', {
+        'hitType': 'pageview',
+        'page': window.location.href
+      });
+  });
 }
